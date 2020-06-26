0598766c393c739f1b652b29a3d738e4d3642fea
devcrp <devcrp>
2020-06-26 14:26:31 +0200

    Just testing around

---
+++ testing1/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Jander</h1>
+    <b-button variant="danger">Exit</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
